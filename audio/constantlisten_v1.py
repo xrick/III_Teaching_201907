1844c80a2cc5dcb57375ac2ce2e57159ebe7e0a9
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2019-08-03 15:43:06 +0800

    Add files via upload

---
+++ audio/constantlisten_v1.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import pyaudio
+
+def EnterConstantListen():
+    mic = sr.Microphone()
+    r = sr.Recognizer()
+    r.energy_threshold = 400
+    print("開始無限收音，請講話")
+    while 1:
+        with mic as audio_source:
+            speech_clip = r.listen(audio_source,timeout=4)
+            regres = r.recognize_google(speech_clip,language='zh-TW')
+            if regres=="離開程式":
+                print("謝謝使用。")
+                break
+            else:
+                print(regres)
+                print("請繼續講話")
+            
+            
+
+def main():
+    EnterConstantListen()
+
+
+if __name__ == "__main__":
+    main()
