ab9b437728ed0731673559b8a3a5b1b51431696c
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2019-08-03 14:16:51 +0800

    Add files via upload

--- audio/audiotest.py
+++ audio/audiotest.py
@@ -1,6 +1,7 @@
 import pyaudio
 import speech_recognition as sr
 from speech_recognition import Recognizer
+from speech_recognition import Microphone
 
 AUDIONAME='harvard.wav'
 NOISEAUDIONAME='jackhammer.wav'
@@ -32,12 +33,20 @@ def googleRecognitionNoise(audioname):
         audio = r.record(source)
         print(r.recognize_google(audio))
 
-
+def detectMicrophone():
+    mic = sr.Microphone()
+    #print(Microphone.list_microphone_names())
+    with mic as audio_source:
+        r = sr.Recognizer()
+        print("Start to listen pls say something:")
+        audio = r.listen(audio_source)
+        print(r.recognize_google(audio,language='zh-TW'))
 
 def apprun(audio_name):
    #googleRecognition(audio_name)
    #googleRecognitionOffSet(audio_name)
-   googleRecognitionNoise(audio_name)
+   #googleRecognitionNoise(audio_name)
+   detectMicrophone()
 
 
 if __name__ == "__main__":
